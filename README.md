16a5a547c60478709c4e0c6fce01215837d1a462
canfei <canfeit@qq.com>
2016-05-12 12:11:47 +0800

    'v0.0.1'

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # meteor-on-openshift
 meteor-on-openshift
 ```bash
-git update-index --chmod=+x .openshift/action_hooks/deploy
+git add . 
+git update-index --chmod=+x .openshift/action_hooks/build
+git update-index --chmod=+x .openshift/action_hooks/pre_build
 git add . && git commit -m 'v0.0.1' && git push
 ```
--- meteorshim.js
+++ meteorshim.js
@@ -2,22 +2,20 @@
 
 // Setup env
 var APP_DNS;
-APP_DNS = "wuwei.kedo.so";
+//APP_DNS = "wuwei.kedo.so";
 
 process.env.ROOT_URL = "http://"+(APP_DNS || process.env.OPENSHIFT_APP_DNS);
 process.env.MONGO_URL = (process.env.OPENSHIFT_MONGODB_DB_URL + process.env.OPENSHIFT_APP_NAME) || "mongodb://localhost:27017/meteor";
-
-// process.env.MAIL_URL = "smtp://email:password@smtp.gmail.com:465";
 process.env.PORT = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 process.env.BIND_IP = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
-process.env.DDP_DEFAULT_CONNECTION_URL = 'http://' + (APP_DNS || process.env.OPENSHIFT_APP_DNS) + ':8000';
-//for ssl
-//process.env.DDP_DEFAULT_CONNECTION_URL = 'https://' + process.env.OPENSHIFT_APP_DNS + ':8443'
+// process.env.MAIL_URL = "smtp://email:password@smtp.gmail.com:465";
 
-// Show connection details on startup
-console.log("MONGO_URL IS: " + process.env.MONGO_URL);
-console.log("ROOT_URL IS: " + process.env.ROOT_URL);
+console.log("#####################################################");
+console.log("ROOT_URL: " + process.env.ROOT_URL);
+console.log("MONGO_URL: " + process.env.MONGO_URL);
 console.log("PORT: " + process.env.PORT);
 console.log("BIND_IP: " + process.env.BIND_IP);
-console.log("DDP_URL: " + process.env.DDP_DEFAULT_CONNECTION_URL);
+console.log("MAIL_URL: " + process.env.MAIL_URL);
+console.log("Node_Version: " + process.version);
+console.log("#####################################################");
 require('./bundle/main.js');
